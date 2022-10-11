94adccb8b64b5790f1eb0428e638da78c6014b3a
EricJin2002 <769327841@qq.com>
2022-10-11 13:31:35 +0800

    fix: updata mul.v

--- mul.v
+++ mul.v
@@ -6,6 +6,7 @@ module mul(
     // output cmp
 );
     // wire [67:0]  cmpans;
+    // wire [67:0]  cmpans2;
     // wire [67:0]  A_cmp;
     // wire [67:0]  B_cmp;
     // assign A_cmp = {{34{mul1[33]}},mul1};
@@ -14,16 +15,14 @@ module mul(
     // assign cmpans = A_cmp*B_cmp;
     // assign cmp = cmpans == ans;
 
-    wire [33: 0] A;  
-    wire [33: 0] A_w;   
-    wire [33: 0] A_2; 
+    wire [33: 0] A;    
+    wire [34: 0] A_2; 
     wire [33: 0] revA;
-    wire [33: 0] rev2A;
+    wire [34: 0] rev2A;
     wire [33: 0] B;
-    wire [33: 0] B_w;
     wire [67: 0] Nsum [16: 0];
-    wire [68: 0] Csum [17: 0];
-    wire [67: 0] Ssum [17: 0];
+    wire [68: 0] Csum [16: 0];
+    wire [67: 0] Ssum [16: 0];
     wire [ 2: 0] y[33: 0];
     assign A = mul1;
     assign revA = ~A;
@@ -44,7 +43,7 @@ module mul(
                                 {68{y[2*i-1]==3'b001}} & {{36-2*i{    A[33]}},    A,{2*i-2{1'b0}}}      |
                                 {68{y[2*i-1]==3'b010}} & {{36-2*i{    A[33]}},    A,{2*i-2{1'b0}}}      |
                                 {68{y[2*i-1]==3'b011}} & {{36-2*i{    A[33]}},    A,{2*i-1{1'b0}}}      |
-                                {68{y[2*i-1]==3'b100}} & {{36-2*i{rev2A[33]}},rev2A,{2*i-2{1'b1}}}      |
+                                {68{y[2*i-1]==3'b100}} & {{35-2*i{rev2A[34]}},rev2A,{2*i-2{1'b1}}}      |
                                 {68{y[2*i-1]==3'b101}} & {{36-2*i{ revA[33]}}, revA,{2*i-2{1'b1}}}      |
                                 {68{y[2*i-1]==3'b110}} & {{36-2*i{ revA[33]}}, revA,{2*i-2{1'b1}}}      |
                                 {68{y[2*i-1]==3'b111}} & 68'b0                                          ;
@@ -61,7 +60,7 @@ module mul(
             addr  addr2(.a(Nsum[  6][i-1]),.b(Nsum[  7][i-1]),.cin(Nsum[  8][i-1]),.s(Ssum[ 2][i-1]),.cout(Csum[ 2][i]));
             addr  addr3(.a(Nsum[  9][i-1]),.b(Nsum[ 10][i-1]),.cin(Nsum[ 11][i-1]),.s(Ssum[ 3][i-1]),.cout(Csum[ 3][i]));
             addr  addr4(.a(Nsum[ 12][i-1]),.b(Nsum[ 13][i-1]),.cin(Nsum[ 14][i-1]),.s(Ssum[ 4][i-1]),.cout(Csum[ 4][i]));
-            addr  addr5(.a(Nsum[ 15][i-1]),.b(Nsum[ 16][i-1]),.cin(1'b0          ),.s(Ssum[ 5][i-1]),.cout(Csum[ 5][i]));
+            addr  addr5(.a(Nsum[ 15][i-1]),.b(1'b0          ),.cin(1'b0          ),.s(Ssum[ 5][i-1]),.cout(Csum[ 5][i]));
             addr  addr6(.a(Ssum[  0][i-1]),.b(Ssum[  1][i-1]),.cin(Ssum[  2][i-1]),.s(Ssum[ 6][i-1]),.cout(Csum[ 6][i]));
             addr  addr7(.a(Ssum[  3][i-1]),.b(Ssum[  4][i-1]),.cin(Ssum[  5][i-1]),.s(Ssum[ 7][i-1]),.cout(Csum[ 7][i]));
             addr  addr8(.a(Csum[  0][i-1]),.b(Csum[  1][i-1]),.cin(Csum[  2][i-1]),.s(Ssum[ 8][i-1]),.cout(Csum[ 8][i]));
@@ -75,7 +74,8 @@ module mul(
             addr addr16(.a(Ssum[ 15][i-1]),.b(Csum[ 14][i-1]),.cin(Csum[ 15][i-1]),.s(Ssum[16][i-1]),.cout(Csum[16][i]));
         end
     endgenerate
-    assign ans = (Ssum[16] + Csum[16]);
+    assign ans = (Ssum[16] + Csum[16] + Nsum[16]);
+    //assign cmpans2 = Nsum[0] + Nsum[1] + Nsum[2] + Nsum[3] + Nsum[4] + Nsum[5] + Nsum[6] + Nsum[7] + Nsum[8] + Nsum[9] + Nsum[10] + Nsum[11] + Nsum[12] + Nsum[13] + Nsum[14] + Nsum[15] + Nsum[16];
 endmodule
 
 module addr(
